032ec83b6fc4a02b172133afd6cbe06e5a4c0704
Erin Polley <erinepolley@gmail.com>
2019-10-15 15:20:50 -0500

    Initial file structure and fetch request set up

---
+++ scripts/foodapi.js
@@ -0,0 +1,14 @@
+console.log("Hi")
+
+fetch("http://localhost:8088/food")
+.then(foods => foods.json()) //got rid of curly brackets here 
+//so you don't have to return this. Shrunk it to one line. 
+//The J
+.then(parsedFoods => {
+    console.table(parsedFoods)
+    // parsedFoods.forEach(food => {
+    //     console.log("food", food)
+    //     console.log(food.name)
+    // })
+    // console.log(parsedFoods)
+}); 
