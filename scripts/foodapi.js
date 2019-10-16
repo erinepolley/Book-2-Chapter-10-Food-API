8988c628d9c511ad67120d9df715b90a7a0378f5
Erin Polley <erinepolley@gmail.com>
2019-10-16 09:09:30 -0500

    started on last exercise

--- scripts/foodapi.js
+++ scripts/foodapi.js
@@ -12,8 +12,8 @@ console.log("Hi")
 //     // })
 //     // console.log(parsedFoods)
 // })
-const foodListDiv = document.querySelector("div")
-foodListDiv.classList.add("foodList")
+const foodListArticle = document.querySelector("article")
+foodListArticle.classList.add("foodList")
 
 // const articleFlexbox = document.querySelector("article")
 // articleFlexbox.classList.add("foodListFlexbox")
@@ -44,10 +44,36 @@ const foodFactory = (foodObj) => {
         })
     })
 
-    linguine: 00286565
-    curry: 0016229008635
-    green curry: 0016229906191 
-    ravioli: 0719283590596
-    tuscan soup: 0011150479547
-    spaghetti: 0071300055557
+//getting new information using the barcodes and 
+//external API.
+
+const foodIngredientDiv = document.querySelectorAll("div")
+foodIngredientDiv.classList.add("dishList")
+
+//     let ingredientItems = ""
+// const populateIngredients = (ingred) => {
+//  return ingredientItems +=  `
+//         <div>
+//             <h3>Name: ${foodObj.name}</h3>
+//             <p>Category: ${foodObj.category}</p>
+//             <p>Ethnicity: ${foodObj.ethnicity}</p>
+//         </div>
+//     `
+//   }
+
+//   const addFoodToDom = (foodToAdd) => {
+//         const foodStuff = document.querySelector(".foodList")
+//         foodStuff.innerHTML = foodToAdd
+//   }
+
+//     fetch("https://world.openfoodfacts.org/api/v0/product/00286565.json")
+//     .then(response => response.json())
+//     .then(productInfo => {
+//         productInfo.forEach(thing =>{
+//             let stuffinDiv = stuff(thing)
+//             addFoodToDom(stuffinDiv)
+//         })
+//     })
+
+
 
