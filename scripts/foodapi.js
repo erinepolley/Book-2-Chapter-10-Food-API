6fdec7cf2ed2fe6e73dbf67c4548a32095eab97b
Erin Polley <erinepolley@gmail.com>
2019-10-15 20:59:51 -0500

    wrote the two functions, made CSS and linked to Index.html

--- scripts/foodapi.js
+++ scripts/foodapi.js
@@ -1,17 +1,40 @@
 console.log("Hi")
 
-fetch("http://localhost:8088/food")
-.then(foods => foods.json()) //got rid of curly brackets here 
-//so you don't have to return this. Shrunk it to one line. 
-//The J
-.then(parsedFoods => {
-    console.table(parsedFoods)
-    // parsedFoods.forEach(food => {
-    //     console.log("food", food)
-    //     console.log(food.name)
-    // })
-    // console.log(parsedFoods)
-})
+// fetch("http://localhost:8088/food")
+// .then(foods => foods.json()) //got rid of curly brackets here 
+// //so you don't have to return this. Shrunk it to one line. 
+// //The J
+// .then(parsedFoods => {
+//     console.table(parsedFoods)
+//     // parsedFoods.forEach(food => {
+//     //     console.log("food", food)
+//     //     console.log(food.name)
+//     // })
+//     // console.log(parsedFoods)
+// })
 const foodListDiv = document.querySelector("div")
 foodListDiv.classList.add("foodList")
 
+let foodListItems = ""
+const foodFactory = (foodObj) => {
+ return foodListItems +=  ` 
+       <h1>Name: ${foodObj.name}</h1>
+        <p>Category: ${foodObj.category}</p>
+        <p>Ethnicity: ${foodObj.ethnicity}</p>
+    `
+  }
+
+  const addFoodToDom = (foodToAdd) => {
+        const foodStuff = document.querySelector(".foodList")
+        foodStuff.innerHTML = foodToAdd
+  }
+
+  fetch("http://localhost:8088/food")
+    .then(foods => foods.json())
+    .then(parsedFoods => {
+        parsedFoods.forEach(food => {
+            let foodAsHTML = foodFactory(food)
+    //        console.log("FOOD AS HTML", foodAsHTML);
+            addFoodToDom(foodAsHTML)
+        })
+    })
